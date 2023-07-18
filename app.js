06dbbaf31b9e946de39a78f9aaf32e8cf6e6e033
Arturo Salinas <prico.team@gmail.com>
2023-07-18 09:56:09 -0600

    implementing debbuging tools (morgan, chalk and debug) to express server

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 
+const chalk = require('chalk');
+
+const debug = require('debug')('app');
+const morgan = require('morgan');
+
 const app = express();
 
+app.use(morgan('tiny'));
+
 app.get('/',(req,res)=>{
-    res.send("Hello World!!")
+    res.send("Hello World!!");
 });
 
 app.listen(3000,()=>{
-    console.log("Server running and listing on port 3000");
+    debug(`Server running and listing on port: ${chalk.green(3000)}` );
 });
