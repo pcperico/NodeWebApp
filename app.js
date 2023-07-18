42cdd96c993849639ee7235c48e7ffd820472f13
Arturo Salinas <prico.team@gmail.com>
2023-07-18 10:26:58 -0600

    adding index.html static file

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require('express');
-
 const chalk = require('chalk');
-
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 
 app.use(morgan('tiny'));
-
+app.use(express.static(path.join(__dirname,'/public')));
 app.get('/',(req,res)=>{
     res.send("Hello World!!");
 });
