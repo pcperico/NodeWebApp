e760f5cda00eb95158e03dd8768db51c567c4849
asalinas1988 <arturo.salinas@staytuned.digital>
2023-07-19 07:53:14 -0600

    Isolating routes withing routes files

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const path = require('path');
 
 const app = express();
 
-const sessionsRouter = express.Router();
+
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname,'/public')));
@@ -16,20 +16,9 @@ app.use(express.static(path.join(__dirname,'/public')));
 app.set('views','./src/views');
 app.set('view engine','ejs');
 
-const sessions = require('./src/data/sessions.json');
 
-sessionsRouter.route('/')
-    .get((req,res)=>{
-        res.render('sessions',{
-            sessions 
-        });
-    });
+const sessionsRouter = require('./src/routers/sessionsRouter');
 
-sessionsRouter.route('/:id')
-    .get((req,res)=>{
-        const id = req.params.id;
-        res.render('session',{session:sessions[id]});
-    });
 
 app.use('/sessions',sessionsRouter);
 
